baa320aeed681c0d98360a0f295234349d9a1fbf
Frazer Robinson <frobinson05@qub.ac.uk>
2019-09-14 21:37:25 +0100

    fix manifest

--- Package.swift
+++ Package.swift
@@ -1,25 +1,19 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
   name: "SwiftArgs",
+  platforms: [
+    .macOS(.v10_14),
+  ],
   products: [
-    // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    .library(
-      name: "SwiftArgs",
-      targets: ["SwiftArgs"]),
+    .library(name: "SwiftArgs", targets: ["SwiftArgs"]),
   ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-  ],
+  dependencies: [],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "SwiftArgs",
-      dependencies: []),
+    .target(name: "SwiftArgs", dependencies: []),
+    .testTarget(
+       name: "Tests",
+       dependencies: ["SwiftArgs"]),
   ]
 )
